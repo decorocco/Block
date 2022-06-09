fced6a236090959a089f2bb4e9792267b7b33887
decorocco <decorocco9@hotmail.com>
2022-06-09 18:21:38 -0300

    office & death

---
+++ Assets/_Scripts/DeathHitbox.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathHitbox : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        GameManager.Instance.ChangeState(GameState.TitleMenu);
+    }
+}
